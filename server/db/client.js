e08f54de37288827f741ffc1c1828a3c4c4254b3
Lucas Montenegro <lmp.montenegro@gmail.com>
2026-03-31 18:53:09 -0300

    Fix database config for Render deploy

--- server/db/client.js
+++ server/db/client.js
@@ -1,4 +1,6 @@
 import pg from "pg";
-const url = process.env.DATABASE_URL || "postgres://example:password@localhost:5432/greetings"
+const url =
+  process.env.DATABASE_URL ||
+  "postgres://localhost:5432/greetings_evxu";
 const db = new pg.Client(url);
 export default db;
